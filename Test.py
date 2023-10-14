0ea1456b31a528f74b30ada88d120dfec3d3fdd2
sgolde13 <sgolde13@jhu.edu>
2023-10-13 23:59:25 -0400

    Test 2

--- Test.py
+++ Test.py
@@ -6,3 +6,27 @@ Created on Fri Oct 13 23:37:31 2023
 @author: shelbygolden
 """
 
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+'''Randomly produce 100 Bernoilli trials 20 times'''
+
+sns.distplot(random.binomial(n = 100, p = 0.5, size = 20)/100, 
+             hist = False, label = 'p=0.5')
+
+sns.distplot(random.binomial(n = 100, p = 0.56, size = 20)/100,
+             hist = False, label = 'p=0.56')
+
+sns.distplot(random.binomial(n = 100, p = 0.7, size = 20)/100,
+             hist = False, label = 'p=0.7')
+
+sns.distplot(random.binomial(n = 100, p = 0.4, size = 20)/100,
+             hist = False, label = 'p=0.4')
+
+plt.title('Resulting Histograms')
+plt.ylabel('Density')
+plt.xlabel('p')
+plt.legend()
+plt.show()
