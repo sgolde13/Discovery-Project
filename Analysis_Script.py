4f62b3afec002256242b403d5b7df9742fb43436
sgolde13 <sgolde13@jhu.edu>
2023-10-14 13:35:56 -0400

    Add the python file

---
+++ Analysis_Script.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 14 13:31:08 2023
+For Simulation Biology EN.605.755.81.FA23
+Assigment 6 Problem 2
+
+@author: Shelby Golden and Mukul Sherekar
+"""
+
